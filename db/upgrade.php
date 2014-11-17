fbc2c4562bb5249beb42e593999919b56dcddc08
vinhub <vin.bhalerao@gmail.com>
2014-11-17 13:10:56 -0800

    split local onenote plugin into msaccount and onenote plugins

---
+++ db/upgrade.php
@@ -0,0 +1,66 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @param int $oldversion the version we are upgrading from
+ * @return bool result
+ */
+function xmldb_local_msaccount_upgrade($oldversion) {
+    global $CFG, $DB;
+
+    $dbman = $DB->get_manager();
+
+    if ($oldversion < 2014111702) {
+        // Define table to be created.
+        $table = new xmldb_table('msaccount_refresh_tokens');
+    
+        // Adding fields to table
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('user_id', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
+        $table->add_field('refresh_token', XMLDB_TYPE_CHAR, '500', null, null, null, null);
+    
+        // Adding keys to table
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
+    
+        // create table
+        if ($dbman->table_exists($table))
+            $dbman->drop_table($table);
+
+        $dbman->create_table($table);
+    
+        // msaccount savepoint reached.
+        upgrade_plugin_savepoint(true, 2014111702, 'local', 'msaccount');
+    }
+    
+    // Moodle v2.3.0 release upgrade line
+    // Put any upgrade step following this
+
+    // Moodle v2.4.0 release upgrade line
+    // Put any upgrade step following this
+
+
+    // Moodle v2.5.0 release upgrade line.
+    // Put any upgrade step following this.
+
+
+    // Moodle v2.6.0 release upgrade line.
+    // Put any upgrade step following this.
+
+    // Moodle v2.7.0 release upgrade line.
+    // Put any upgrade step following this.
+
+    return true;
+}
---
+++ version.php
@@ -0,0 +1,27 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version details for msaccount local plugin
+ *
+ * @package    local_msaccount
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2014111702;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2014050800;        // Requires this Moodle version.
+$plugin->component = 'local_msaccount'; // Full name of the plugin (used for diagnostics).
